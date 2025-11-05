c91110ad475cd6a12ea6f7d4e295903d3531d038
Junseo Bae <ferrater1013@gmail.com>
2025-11-05 20:57:47 +0900

    feat: CORS 허용

--- smartmealtable-api/src/main/java/com/stdev/smartmealtable/api/config/WebMvcConfig.java
+++ smartmealtable-api/src/main/java/com/stdev/smartmealtable/api/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import com.stdev.smartmealtable.api.config.resolver.AuthUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -21,5 +22,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
